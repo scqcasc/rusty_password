c57cdfb959a7ada5770769de2b175ebaadd30c2c
Sean Quaint <seanquaint@zohomail.com>
2024-07-17 20:58:19 -0400

    added gtk

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ fn main() {
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789)(*&^%$#@!~{}[]+=";
-    let PASSWORD_LEN: i32 = length.parse().expect("message");
+    let password_len: i32 = length.parse().expect("message");
     let mut rng = rand::thread_rng();
 
-    let password: String = (0..PASSWORD_LEN)
+    let password: String = (0..password_len)
         .map(|_| {
             let idx = rng.gen_range(0..CHARSET.len());
             CHARSET[idx] as char
