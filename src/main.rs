d5df410b0b944fccb9d4944ecadb8be40b5179f4
= <=>
2024-07-19 13:54:51 -0400

    added clap option handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 use std::env;
 use gtk::prelude::*;
 use gtk::{glib, Application};
+use clap::Parser;
 
 const APP_ID: &str = "org.scq.Rusty_Password";
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let length = &args[1];
+///Set up cli arg
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args{
+    /// length of password to create
+    #[arg(short, long, default_value_t = 15)]
+    length: i32,
+}
+
+fn main() { 
+    let empty: Vec<String> = vec![];
+    let args = Args::parse();
     let app = Application::builder().application_id(APP_ID).build();
-    app.run();
+    app.run_with_args(&empty);
 
     use rand::Rng;
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789)(*&^%$#@!~{}[]+=";
-    let password_len: i32 = length.parse().expect("message");
+    let password_len = args.length;
     let mut rng = rand::thread_rng();
 
     let password: String = (0..password_len)
