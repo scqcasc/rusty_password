9c100809c1ce8cd07ebba80931e6ef7cbf7ad4cc
Sean Quaint <seanquaint@zohomail.com>
2024-12-09 17:05:57 -0500

    added radio buttons

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 // Thanks to https://github.com/pjmlp/gwc-rs/ for showing the way forward with the GUI
 
-
 extern crate gtk;
 
+use clap::Parser;
 use gtk::glib::Propagation;
 use gtk::prelude::*;
-use gtk::{Window, WindowType, Label, Menu, MenuBar, MenuItem, IconSize, Image, AboutDialog, Toolbar, ToolButton,
-    ToolbarStyle, SeparatorToolItem};
-use clap::Parser;
+use gtk::{
+    AboutDialog, IconSize, Image, Label, Menu, MenuBar, MenuItem, SeparatorToolItem, ToolButton,
+    Toolbar, ToolbarStyle, Window, WindowType,
+};
 use password::PasswordType;
-use std::rc::Rc;
 use std::borrow::Borrow;
+use std::rc::Rc;
 
 mod password;
-
 
 ///Set up cli arg
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args{
+struct Args {
     /// length of password to create
     #[arg(short, long, default_value_t = 15)]
     length: i32,
@@ -40,7 +40,7 @@ struct GtkPasswordTypes {
 #[derive(Debug)]
 // // probably don't need this
 struct GtkPasswdArray {
-    types: Vec<GtkPasswordTypes>  
+    types: Vec<GtkPasswordTypes>,
 }
 
 /// The application window state
@@ -50,19 +50,21 @@ struct GWCApp {
     passwd_label: Option<Rc<Label>>,
 
     /// a kind of handle for the Gtk+ window
-    window : Option<Rc<Window>>,
+    window: Option<Rc<Window>>,
 
     /// option for password type
     password_type: Option<Rc<GtkPasswdArray>>,
 }
 
 impl GWCApp {
-
     /// Provides a new instance of the GWC application
     pub fn new() -> GWCApp {
-        GWCApp { passwd_label: None, window: None, password_type: None}
+        GWCApp {
+            passwd_label: None,
+            window: None,
+            password_type: None,
+        }
     }
-    
 
     pub fn get_pass_type(data: &Rc<GtkPasswdArray>) -> PasswordType {
         for pw_type in &data.types {
@@ -74,14 +76,13 @@ impl GWCApp {
         /* `Option<PasswordType>` value */
     }
 
-    pub fn set_password(_win:&Rc<Window>, lbl : &Rc<Label>, pass_type: PasswordType) {
-        
+    pub fn set_password(_win: &Rc<Window>, lbl: &Rc<Label>, pass_type: PasswordType) {
         let p = password::Password {
             password_type: pass_type,
             password_length: 15,
         };
         let pass_str: String = p.get_a_password();
-        let pass_mu: String= format!("<span size='16pt'>{}</span>",&pass_str);
+        let pass_mu: String = format!("<span size='16pt'>{}</span>", &pass_str);
         lbl.set_selectable(true);
         lbl.set_focus_on_click(true);
         lbl.set_markup(pass_mu.as_str());
@@ -91,25 +92,33 @@ impl GWCApp {
     /// Responsible for initializing the application state, including the whole UI
     pub fn init(&mut self) {
         let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
-        let  password = Label::new(None);
+        let password = Label::new(None);
         let win = Window::new(WindowType::Toplevel);
         let rbgs: Vec<GtkPasswordTypes> = self.build_rbg();
-        let rbgs_array: GtkPasswdArray = GtkPasswdArray{ types: rbgs};
+        let rbgs_array: GtkPasswdArray = GtkPasswdArray { types: rbgs };
         win.set_title("Rusty Password");
         win.set_position(gtk::WindowPosition::Center);
         win.set_size_request(500, 400);
         win.connect_delete_event(|_, _| {
             gtk::main_quit();
-            Propagation::Stop  
+            Propagation::Stop
         });
 
         // The fields must be updated for the helper methods to work as expected.
         self.window = Some(Rc::new(win));
         self.passwd_label = Some(Rc::new(password));
         self.password_type = Some(Rc::new(rbgs_array));
-        
-        let pt: PasswordType= GWCApp::get_pass_type(&<Option<Rc<GtkPasswdArray>> as Clone>::clone(&self.password_type).unwrap().clone());
-        GWCApp::set_password(&self.window.clone().unwrap(), &self.passwd_label.clone().unwrap(), pt);
+
+        let pt: PasswordType = GWCApp::get_pass_type(
+            &<Option<Rc<GtkPasswdArray>> as Clone>::clone(&self.password_type)
+                .unwrap()
+                .clone(),
+        );
+        GWCApp::set_password(
+            &self.window.clone().unwrap(),
+            &self.passwd_label.clone().unwrap(),
+            pt,
+        );
 
         // create the application menu
         let menu_bar = self.init_menus();
@@ -124,11 +133,11 @@ impl GWCApp {
         v_box.pack_start(&radio_container, false, true, 0);
 
         // Create the password label
-        if let Some (ref lbl) = self.passwd_label {
+        if let Some(ref lbl) = self.passwd_label {
             v_box.pack_start(lbl.as_ref(), true, true, 0);
         }
-        
-        if let Some (ref w) = self.window {
+
+        if let Some(ref w) = self.window {
             w.add(&v_box);
         }
     }
@@ -143,7 +152,7 @@ impl GWCApp {
     }
 
     /// Creates the application menus
-    fn init_menus (&self) -> MenuBar {
+    fn init_menus(&self) -> MenuBar {
         let menu = Menu::new();
         let menu_bar = MenuBar::new();
         let file = MenuItem::with_label("File");
@@ -167,10 +176,14 @@ impl GWCApp {
         menu_bar.append(&help);
 
         // This connects the new_passwd menu item with the set_password method
-        if let Some (ref label) = self.passwd_label {
-            if let Some (ref win) = self.window {
+        if let Some(ref label) = self.passwd_label {
+            if let Some(ref win) = self.window {
                 // let pt: Vec<GtkPasswordTypes> = self.build_rbg();
-                let pt: PasswordType= GWCApp::get_pass_type(&<Option<Rc<GtkPasswdArray>> as Clone>::clone(&self.password_type).unwrap().clone());
+                let pt: PasswordType = GWCApp::get_pass_type(
+                    &<Option<Rc<GtkPasswdArray>> as Clone>::clone(&self.password_type)
+                        .unwrap()
+                        .clone(),
+                );
                 let w = win.to_owned().clone();
                 let l = label.clone();
                 new_passwd.connect_activate(move |_| {
@@ -207,7 +220,7 @@ impl GWCApp {
     }
 
     /// Creates a vector of radio butttons for the form
-    pub fn build_rbg(&self) -> Vec<GtkPasswordTypes> { 
+    pub fn build_rbg(&self) -> Vec<GtkPasswordTypes> {
         let rbc: GtkPasswordTypes = GtkPasswordTypes {
             radio_buton: gtk::RadioButton::with_label("complex"),
             password_type: PasswordType::Complex,
@@ -223,7 +236,7 @@ impl GWCApp {
         rbg.push(rbs);
         rbg
     }
-    
+
     // Create the extra tools toolbar
     fn init_extra_tools(&self) -> gtk::Box {
         let toolbar: gtk::Box = gtk::Box::new(gtk::Orientation::Vertical, 0);
@@ -239,24 +252,29 @@ impl GWCApp {
         let toolbar = Toolbar::new();
         toolbar.set_style(ToolbarStyle::Both);
 
-        let new_pass_button_image = Image::from_icon_name(Some("document-new"), IconSize::LargeToolbar.into());
+        let new_pass_button_image =
+            Image::from_icon_name(Some("document-new"), IconSize::LargeToolbar.into());
         let new_pass_button = ToolButton::new(Some(&new_pass_button_image), Some("New"));
         toolbar.insert(&new_pass_button, 0);
 
         let sep = SeparatorToolItem::new();
         toolbar.insert(&sep, 1);
 
-        let quit_btn_image = Image::from_icon_name(Some("application-exit"), IconSize::LargeToolbar.into());
+        let quit_btn_image =
+            Image::from_icon_name(Some("application-exit"), IconSize::LargeToolbar.into());
         let quit_btn = ToolButton::new(Some(&quit_btn_image), Some("Quit"));
         toolbar.insert(&quit_btn, 2);
 
-
         // This connects the new_passwd menu item with the set_password method
-        if let Some (ref label) = self.passwd_label {
-            if let Some (ref win) = self.window {
+        if let Some(ref label) = self.passwd_label {
+            if let Some(ref win) = self.window {
                 // let pt: Vec<GtkPasswordTypes> = self.build_rbg();
-                let pt: PasswordType= GWCApp::get_pass_type(&<Option<Rc<GtkPasswdArray>> as Clone>::clone(&self.password_type).unwrap().clone());
-                
+                let pt: PasswordType = GWCApp::get_pass_type(
+                    &<Option<Rc<GtkPasswdArray>> as Clone>::clone(&self.password_type)
+                        .unwrap()
+                        .clone(),
+                );
+
                 let w = win.to_owned().clone();
                 let l = label.clone();
                 new_pass_button.connect_clicked(move |_| {
@@ -273,8 +291,7 @@ impl GWCApp {
     }
 }
 
-
-fn main() { 
+fn main() {
     let args = Args::parse();
     if args.gui {
         if gtk::init().is_err() {
@@ -285,7 +302,7 @@ fn main() {
         window.init();
         window.show();
         gtk::main();
-    }else{
+    } else {
         let p = password::Password {
             password_type: {
                 if args.simple {
@@ -299,5 +316,4 @@ fn main() {
         let my_password: String = p.get_a_password();
         println!("{:?}", my_password);
     }
-    
 }
